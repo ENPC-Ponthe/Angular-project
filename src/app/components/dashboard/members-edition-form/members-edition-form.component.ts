ce3b2af40d38687dd5d59b9486f31ab6e1c552ac
Thomas Chabal <t.chabal@hotmail.fr>
2021-02-25 21:30:42 +0100

    Finished members edition form

--- src/app/components/dashboard/members-edition-form/members-edition-form.component.ts
+++ src/app/components/dashboard/members-edition-form/members-edition-form.component.ts
@@ -12,109 +12,6 @@ const DURATION_DISPLAYING = 6000;
 })
 export class MembersEditionFormComponent implements OnInit {
   teamPonthe = [];
-  teamPontheRaw = {
-    "team_ponthe": [
-        {
-            "year": "022",
-            "year_id": "p022",
-            "members": [
-                "Président : Adrien Sadé",
-                "Vice-Présidente Images : Louise Eymery",
-                "Vice-Présidente Création : Sarah Ropert",
-                "Trésorier : Jean-Paul Eisenmann",
-                "Respo Communication : Lise Dousset",
-                "Respo Matériel : Cyril Nederveen",
-                "Respo Formation : Brieuc Valokuvaaja",
-                "Respo Drone : Hadrien Thébault",
-                "Respos Retouche : Romain Chaussonnier, Valentin Cheval",
-                "Monteurs : Emma-Jane Touhey, Victor Guichard, Juliette Piquois",
-                "Photographes : Coline Mazas, Thibaud Cambronne, Clément Daguenet"
-            ],
-            "prev_year_id": "p021"
-        },
-        {
-            "year": "021",
-            "year_id": "p021",
-            "members": [
-                "Présidente : Auriane Riou",
-                "Vice-Présidente : Pauline Hubert",
-                "Trésorière : Amélie Guyot",
-                "Respos Communication : Paule-Claire He-Xie et Raphaël Lasry",
-                "Respo Matériel : Benjamin Sauze",
-                "Pôle montage : Marie Puistienne et Peio Betbeder",
-                "Pôle photo : Maëlle Sardet, Jean-Sébastien Verhaeghe, Clément Norodom, Charlotte Nivelle, Pia Chancerel",
-                "Pôle Créations originales : Augustin Doumic, Benjamin Roulin, Clément Allain, Pol Le-Gurun"
-            ],
-            "prev_year_id": "p020"
-        },
-        {
-            "year": "020",
-            "year_id": "p020",
-            "members": [
-                "Président : Thomas Chabal",
-                "Vice-Présidente : Félicie Levinton",
-                "Respo Images : Quentin Spinat",
-                "Respo Matériel : Martin Graive",
-                "Respos Communication : Candice Pichon et Nathan Godey",
-                "Chefs monteur : Wissam Alami-Messaouidi et Jeffrey Mahou",
-                "Directeur Photo : Philippe d'Harcourt",
-                "Respo Web : Alexandre Perez",
-                "Polaroïd : Clémence Barillot",
-                "Respo International : Rolando Saavedra"
-            ],
-            "prev_year_id": "p019"
-        },
-        {
-            "year": "019",
-            "year_id": "p019",
-            "members": [
-                "Présidente : Inès Tazi",
-                "Responsable Web : Philippe Ferreira de Sousa",
-                "Membres : Francesca Carreiro, Victor Noisette, Thomas Benichou, Stephane Allado, Hugo Sénetaire, Anaïs Chopin, Louis Dumont"
-            ],
-            "prev_year_id": "p018"
-        },
-        {
-            "year": "018",
-            "year_id": "p018",
-            "members": [
-                "Président : Lucien Mauffret",
-                "Vice-Président : Augustin Joncheray",
-                "Secrétaire Général : Anatole Parre",
-                "Photons : Victor Marchais, Nicolas Thery et Aurélie Shi",
-                "Pixels : Alexandre Aheto, Felix Roubaud et Romain Durand"
-            ],
-            "prev_year_id": "p017"
-        },
-        {
-            "year": "017",
-            "year_id": "p017",
-            "members": [
-                "Président : Alban Berrube"
-            ],
-            "prev_year_id": "p016"
-        },
-        {
-            "year": "016",
-            "year_id": "p016",
-            "members": [
-                "Président : Omar Mounir Alaoui",
-                "Réalisateur : Eliot Andres"
-            ],
-            "prev_year_id": "p015"
-        },
-        {
-            "year": "015",
-            "year_id": "p015",
-            "members": [
-                "Président : Geoffroy Gallier",
-                "Membres : Mathieu Ramananarivo, Mickael Bergem, Chanh-Nghi Lam, Paul Chabas et Paul Boosz"
-            ],
-            "prev_year_id": ""
-        }
-    ]
-};
-  fragmentIntro = null;
 
   successEdition = false;
   failureEdition = false;
@@ -135,9 +32,6 @@ export class MembersEditionFormComponent implements OnInit {
       },
       (error) => { console.log(error); }
     );
-    // /!\ TEMPORARY
-    this.teamPonthe = this.teamPontheRaw.team_ponthe;
-    console.log(this.teamPonthe)
   }
 
   arrayToJson(articles: any) {
@@ -158,26 +52,26 @@ export class MembersEditionFormComponent implements OnInit {
     const numberOfTeams = this.teamPonthe.length;
     const indexOfNewTeam = teamIndex == 0 ? 0 : numberOfTeams;
     const year = teamIndex == 0
-      ? "0" + (parseInt(this.teamPonthe[0].year) + 1).toString()
-      : "0" + (parseInt(this.teamPonthe[numberOfTeams - 1].year) - 1).toString();
+      ? '0' + (parseInt(this.teamPonthe[0].year) + 1).toString()
+      : '0' + (parseInt(this.teamPonthe[numberOfTeams - 1].year) - 1).toString();
 
     const emptyTeam = {
-      "year": year,
-      "year_id": "p" + year,
-      "members": [""],
-      "prev_year_id": "p0" + (parseInt(year) - 1).toString()
+      year: year,
+      year_id: 'p' + year,
+      members: [''],
+      prev_year_id: 'p0' + (parseInt(year) - 1).toString()
     };
     this.teamPonthe.splice(indexOfNewTeam, 0, emptyTeam);
   }
 
   addMember(teamIndex: number) {
     const membersList = this.teamPonthe[teamIndex].members;
-    membersList.push("")
+    membersList.push('')
     this.teamPonthe[teamIndex].members = membersList
   }
   
   removeMember(teamIndex: number, memberIndex: number) {
-    let membersList = this.teamPonthe[teamIndex].members;
+    const membersList = this.teamPonthe[teamIndex].members;
     membersList.splice(memberIndex, 1);
     this.teamPonthe[teamIndex].members = membersList
     if (membersList.length === 0) {
@@ -193,9 +87,8 @@ export class MembersEditionFormComponent implements OnInit {
     this.visibleModal = false;
   }
 
-  // Submission of gallery creation
-  onSubmitEvent() {
-    const teamJson = { 'team_ponthe': this.teamPonthe };
+  onSubmitMembers() {
+    const teamJson = { members: { team_ponthe: this.teamPonthe } };
     this.assetsService.editMembers(teamJson).subscribe(
       res => {
         this.successEdition = true;
